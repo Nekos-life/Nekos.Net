f633aaea7a6b3e8066fc910d5588d5f9d938d8b2
Tien Dat Pham <dat20036@gmail.com>
2022-02-14 20:49:50 +0700

    Format fix

---
+++ Nekos.Net/V3/Responses/NekosListedResponse.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Nekos.Net.V3.Responses;
+
+public class NekosListedResponse
+{
+    [JsonProperty("data")]
+    public NekosResponseListData Data;
+    
+    [JsonProperty("status")]
+    public NekosResponseListData Status;
+}
+
+[JsonObject("data")]
+public class NekosResponseListData
+{
+    [JsonProperty("response")]
+    public NekosResponseListDataResponse Response;
+}
+
+[JsonObject("response")]
+public class NekosResponseListDataResponse
+{
+    [JsonProperty("urls")]
+    public List<string> Urls;
+}
+
+[JsonObject("status")]
+public class NekosResponseListStatus
+{
+    [JsonProperty("rendered_in")]
+    public double RenderedIn;
+    
+    [JsonProperty("message")]
+    public int RequestMessage;
+    
+    [JsonProperty("code")]
+    public int StatusCode;
+    
+    [JsonProperty("success")]
+    public bool Success;
+}
---
+++ Nekos.Net/V3/Responses/NekosSingleResponse.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+
+namespace Nekos.Net.V3.Responses;
+
+public class NekosSingleResponse
+{
+    [JsonProperty("data")]
+    public NekosSingleResponseData Data;
+    
+    [JsonProperty("status")]
+    public NekosSingleResponseStatus Status;
+}
+
+[JsonObject("data")]
+public class NekosSingleResponseData
+{
+    [JsonProperty("response")]
+    public NekosSingleResponseDataResponse Response;
+}
+
+[JsonObject("response")]
+public class NekosSingleResponseDataResponse
+{
+    [JsonProperty("url")]
+    public string Url;
+}
+
+[JsonObject("status")]
+public class NekosSingleResponseStatus
+{
+    [JsonProperty("rendered_in")] public double RenderedIn;
+    [JsonProperty("message")] public int RequestMessage;
+    [JsonProperty("code")] public int StatusCode;
+    [JsonProperty("success")] public bool Success;
+}

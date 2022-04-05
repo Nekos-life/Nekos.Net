c229b0ceba2ec293c48887cd367fa140227ad46f
Tien Dat Pham <dat20036@gmail.com>
2022-04-05 14:44:43 +0700

    oops.

--- Nekos.Net/V2/Responses/NekosCat.cs
+++ Nekos.Net/V2/Responses/NekosCat.cs
@@ -5,7 +5,7 @@ namespace Nekos.Net.V2.Responses;
 public class NekosCat
 {
     /// <summary>
-    ///     Why question.
+    ///     ASCII cat text.
     /// </summary>
     [JsonProperty("cat")] public string Cat;
 }
